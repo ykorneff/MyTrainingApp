2ec42f1f9de6e412927cc583fc6655ef03cf25a3
Yury Korneff <korneff@gmail.com>
2019-01-18 17:23:34 +0100

    w-s-t-c
    working simple text chat

---
+++ 02.stChat42/index.js
@@ -0,0 +1,28 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(`${__dirname}/index.html` );
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    //socket.broadcast.emit('hi');
+    socket.on('disconnect', function (){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        console.log(`Message: ${msg}`);
+        io.emit('chat message', msg);
+    })
+});
+
+io.emit('some event', {for: 'evereyone'});
+
+
+
+http.listen(3000, function(){
+    console.log('listening on port 3000');
+})
