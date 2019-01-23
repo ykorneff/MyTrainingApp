4e4c868a766f872fbbf7464ca146de9fbeb5ca58
Yury Kornev <korneff@gmail.com>
2019-01-23 22:47:10 +0100

    aaa

--- 02.stChat42/index.js
+++ 02.stChat42/index.js
@@ -4,10 +4,19 @@ let path = require('path');
 let express = require('express');
 
 let app= express();
-app.use(express.static(path.join(__dirname, 'public')));
-//let app = require('express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+app.use('/styles', express.static('public'));
+console.log(__dirname);
+//let http = require('http').Server(app);
+let fs = require('fs');
+
+let privateKey = fs.readFileSync('key.pem').toString();
+let certificate = fs.readFileSync('cert.pem').toString();
+let credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+let https = require('https').Server(credentials,app);
+let io = require('socket.io')(https);
 
 app.get('/', function(req, res){
     res.sendFile(`${__dirname}/index.html` );
@@ -28,8 +37,12 @@ io.on('connection', function(socket){
 
 io.emit('some event', {for: 'evereyone'});
 
+https.listen(3001, function(){
+    console.log('listening on port 3001');
+})
 
-
+/*
 http.listen(3000, function(){
     console.log('listening on port 3000');
 })
+*/
